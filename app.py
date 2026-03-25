2db422a78187846d7734d5bb3682170b966aad65
ywt <1625208758@qq.com>
2026-03-25 15:50:34 +0800

    完成项目的前端页面

---
+++ app.py
@@ -0,0 +1,48 @@
+import time
+
+import streamlit as st
+
+from agent.react_agent import ReactAgent
+
+st.title('智能客服机器人')
+st.divider()
+# 将react_agent对象保存在session_state中
+if 'agent' not in st.session_state:
+    st.session_state['agent'] = ReactAgent()
+
+# 将聊天消息保存到session_state中
+if 'messages' not in st.session_state:
+    st.session_state['messages'] = []
+
+for message in st.session_state["messages"]:
+    st.chat_message(message["role"]).write(message["content"])
+
+# 用户输入框
+prompt = st.chat_input("请输入问题：")
+
+# ... existing code ...
+
+if prompt:
+    st.chat_message("user").write(prompt)
+    st.session_state["messages"].append({"role": "user", "content": prompt})
+
+    response_messages = []
+    with st.spinner("智能客服思考中..."):
+        res_stream = st.session_state["agent"].execute_stream(prompt)
+
+        def capture(generator, cache_list):
+
+            for chunk in generator:
+                cache_list.append(chunk)
+
+                for char in chunk:
+                    time.sleep(0.01)
+                    yield char
+
+        st.chat_message("assistant").write_stream(capture(res_stream, response_messages))
+        st.session_state["messages"].append({"role": "assistant", "content": response_messages[-1]})
+        st.rerun()
+
+
+
+
